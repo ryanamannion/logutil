5882a97b9ac438b867d49ae8c8f243d98e4fb10f
Ryan A. Mannion <46059412+ryanamannion@users.noreply.github.com>
2021-05-24 16:39:43 -0400

    Adds new functionality
    new functionality includes:
    - more clear config section to change the editor
    - mkdir `logs` if it does not exist
    - if a log exists for that day it will open it

--- newlog.sh
+++ newlog.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
+
+#===============================================================
+# this script creates a new log file and opens it with the 
+# chosen editor
+#===============================================================
+
+##########
+# CONFIG #
+##########
+
+EDITOR=micro
+
+##########
+# SCRIPT #
+##########
+
 PWDOLD=$PWD
-cd ~/logs
+
+cd
+if [ ! -d "./logs" ]; then
+	mkdir "logs"
+fi
+cd logs
+
+# name log based on curent date
 LOGNAME=$(date +"%Y-%m-%d.txt")
-echo "$LOGNAME" | xargs touch
-nano "$LOGNAME"
+
+# creates new log if one does not already exist for that day
+if [ ! -f "./${LOGNAME}" ]; then
+	echo "$LOGNAME" | xargs touch
+fi
+
+# open log with config'd editor
+eval $EDITOR $LOGNAME
+
+# change back to old dir for ~continuity~
 cd $PWDOLD
